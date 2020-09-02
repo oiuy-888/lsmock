a73f1688b2c48ec01f4504c69eba1019b3986a46
oiuy-888 <oiuy-888@163.comm>
2020-09-02 23:22:20 +0800

    增加过滤器配置

--- src/main/java/com/example/lsmock/LsmockApplication.java
+++ src/main/java/com/example/lsmock/LsmockApplication.java
@@ -3,11 +3,13 @@ package com.example.lsmock;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @MapperScan("com.example.lsmock.mapper")
+@ImportResource("classpath:/spring/*.xml")
 public class LsmockApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LsmockApplication.class, args);
--- src/main/java/com/example/lsmock/interceptor/LoginInterceptor.java
+++ src/main/java/com/example/lsmock/interceptor/LoginInterceptor.java
@@ -1,7 +1,41 @@
 package com.example.lsmock.interceptor;
 
+import com.example.lsmock.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
 
-public class LoginInterceptor {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
     //过滤器（filter）和拦截器（interceptor）  过滤前-拦截前-action执行-拦截后-过滤后
+
+    @Autowired
+    private UserServiceImpl userService;
+    private List<String> excludedUrls;
+
+    public void setExcludedUrls(List<String> excludedUrls) {
+        this.excludedUrls = excludedUrls;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse response, Object o) throws Exception {
+        //不过滤的url处理
+        String uri = httpServletRequest.getRequestURI();
+        for (String url : excludedUrls) {
+            if (uri.contains(url)) {
+                return true;
+            }
+        }
+        //需要处理的url
+        String header = httpServletRequest.getHeader("X-token");
+        if(userService.verify(header)){
+            return true;
+        }
+        return false;
+    }
 
 }
