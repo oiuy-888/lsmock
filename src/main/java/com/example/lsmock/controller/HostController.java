6b3b66645b9d87a3d80402f6465dd3ca9f6c1228
lishuai228 <lishuai228@jd.com>
2020-09-23 20:26:03 +0800

    增加异常返回

--- src/main/java/com/example/lsmock/controller/HostController.java
+++ src/main/java/com/example/lsmock/controller/HostController.java
@@ -3,6 +3,7 @@ package com.example.lsmock.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.example.lsmock.dao.Host;
 import com.example.lsmock.service.HostService;
+import com.example.lsmock.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.alibaba.fastjson.JSON;
@@ -24,57 +25,54 @@ public class HostController {
 
 
     @RequestMapping(value="findhost", method = RequestMethod.GET)
-    public String findhost(HttpServletRequest request) throws IOException {
-        request.setCharacterEncoding("utf-8");
-        List<Host> list = hostService.findAll();
-        JSONObject json = new JSONObject();
-        json.put("code",20000);
-        json.put("data",list);
-        return json.toString();
+    public Result findhost(HttpServletRequest request) {
+        try{
+            request.setCharacterEncoding("utf-8");
+            List<Host> list = hostService.findAll();
+            return new Result(Result.Success,Result.SuccessMsg);
+        }catch(Exception e){
+            return new Result(Result.Error,Result.ErrorMsg);
+        }
     }
 
     
     @RequestMapping(value="updatehost", method = RequestMethod.POST)
-    public String updateHost(HttpServletRequest request, @RequestBody List<Host> host) throws IOException {
-        request.setCharacterEncoding("UTF-8");
-        List<Integer> list = hostService.findHostId();
-        List<Integer> dellist = hostService.findHostId();
-        for(int i=0;i<host.size();i++) {
-            if (list.contains(host.get(i).getId())) {
-                hostService.updateHost(host.get(i));
-                dellist.remove(host.get(i).getId());
-            }if(!list.contains(host.get(i).getId())){
-                hostService.addHost(host.get(i));
+    public Result updateHost(HttpServletRequest request, @RequestBody List<Host> host) {
+        try{
+            request.setCharacterEncoding("UTF-8");
+            List<Integer> list = hostService.findHostId();
+            List<Integer> dellist = hostService.findHostId();
+            for(int i=0;i<host.size();i++) {
+                if (list.contains(host.get(i).getId())) {
+                    hostService.updateHost(host.get(i));
+                    dellist.remove(host.get(i).getId());
+                }if(!list.contains(host.get(i).getId())){
+                    hostService.addHost(host.get(i));
+                }
             }
+            for (int i=0;i<dellist.size();i++){
+                hostService.deleteHost(dellist.get(i));
+            }
+            return new Result(Result.Success,Result.SuccessMsg);
+        }catch(Exception e){
+            return new Result(Result.Error,Result.ErrorMsg);
         }
-        for (int i=0;i<dellist.size();i++){
-            hostService.deleteHost(dellist.get(i));
-        }
-        JSONObject json = new JSONObject();
-        json.put("code",20000);
-        json.put("data","更新成功");
-        return json.toString();
     }
 
     @RequestMapping(value="addhost", method = RequestMethod.POST)
-    public String addhost(HttpServletRequest request, @RequestBody List<Host> host) throws IOException {
+    public Result addhost(HttpServletRequest request, @RequestBody List<Host> host) throws IOException {
         request.setCharacterEncoding("UTF-8");
         for (int i=0;i<host.size();i++){
             hostService.addHost(host.get(i));
         }
-        JSONObject json = new JSONObject();
-        json.put("code",20000);
-        json.put("data","添加成功");
-        return json.toString();
+        return new Result(Result.Success,Result.SuccessMsg);
     }
 
     @RequestMapping(value="deletehost", method = RequestMethod.GET)
-    public String deleteHost(HttpServletRequest request, @RequestParam("id") Integer id) throws IOException {
+    public Result deleteHost(HttpServletRequest request, @RequestParam("id") Integer id) throws IOException {
         request.setCharacterEncoding("UTF-8");
         hostService.deleteHost(id);
         JSONObject json = new JSONObject();
-        json.put("code",20000);
-        json.put("data","删除成功");
-        return json.toString();
+        return new Result(Result.Success,Result.SuccessMsg);
     }
 }
