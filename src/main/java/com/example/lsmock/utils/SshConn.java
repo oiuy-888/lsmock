48391db8e12bfc0294148b4698fbe104af5bfd0e
oiuy-888 <oiuy-888@163.comm>
2020-10-22 20:16:33 +0800

    增加连接linux

---
+++ src/main/java/com/example/lsmock/utils/SshConn.java
@@ -0,0 +1,48 @@
+package com.example.lsmock.utils;
+
+import ch.ethz.ssh2.Connection;
+import ch.ethz.ssh2.Session;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.junit.Test;
+
+public class SshConn {
+
+
+    @Test
+    public void linuxssh() throws IOException{
+        String host = "192.168.238.128";
+        String username = "master";
+        String password = "123456";
+        // 创建连接
+        Connection conn = new Connection(host);
+        // 启动连接
+        conn.connect();
+        // 验证用户密码
+        conn.authenticateWithPassword(username, password);
+        Session session = conn.openSession();
+        session.execCommand("pwd");
+
+        // 消费所有输入流
+        String inStr = consumeInputStream(session.getStdout());
+        String errStr = consumeInputStream(session.getStderr());
+        System.out.println(inStr+"/n"+errStr);
+
+        session.close();
+        conn.close();
+    }
+
+    public static String consumeInputStream(InputStream is) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String s ;
+        StringBuilder sb = new StringBuilder();
+        while((s=br.readLine())!=null){
+            System.out.println(s);
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+}
