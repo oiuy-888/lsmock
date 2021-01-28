7fca486d6fa9c7194c74a8e88b87fd64465ee5e3
lishuai228 <lishuai228@jd.com>
2021-01-28 19:09:29 +0800

    计划增加表存储

--- src/main/java/com/example/lsmock/mapper/BasesMapper.java
+++ src/main/java/com/example/lsmock/mapper/BasesMapper.java
@@ -2,6 +2,7 @@ package com.example.lsmock.mapper;
 
 import com.example.lsmock.dao.Bases;
 import com.example.lsmock.dao.Consul;
+import com.example.lsmock.dao.FormSql;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +12,14 @@ import java.util.List;
 @Component
 public interface BasesMapper {
 
+    //数据库
     public List<Bases> findAll();
     public Bases findBases(Bases bases);
     public Integer addBases(Bases bases);
     public void deleteBases(Bases bases);
+
+    //表
+    public Integer addForm(FormSql formSql);
+    public void deleteForm(FormSql formSql);
+
 }
